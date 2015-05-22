d18c0b213018e5d6dc629b563c942af5840d592a
Noriaki Katayama <kataring@gmail.com>
2015-05-22 14:08:13 +0900

    bug fixed

--- test/plugin/test_filter_string-scrub.rb
+++ test/plugin/test_filter_string-scrub.rb
@@ -5,6 +5,7 @@ class StringScrubFilterTest < Test::Unit::TestCase
 
   def setup
     Fluent::Test.setup
+    @time = Fluent::Engine.now
   end
 
   CONFIG = %[
@@ -23,22 +24,25 @@ class StringScrubFilterTest < Test::Unit::TestCase
     Fluent::Test::FilterTestDriver.new(Fluent::StringScrubFilter).configure(conf, tag)
   end
 
+  def filter(config, msgs)
+    d = create_driver(config)
+    d.run {
+      msgs.each {|msg|
+        d.filter(msg, @time)
+      }
+    }
+    filtered = d.filtered_as_array
+    filtered.map {|m| m[2] }
+  end
+
   def test_filter1
     return unless defined? Fluent::Filter
-    return if RUBY_VERSION.to_f < 2.1
 
     orig_message = 'testtesttest'
     invalid_utf8 = "\xff".force_encoding('UTF-8')
-
-    es = Fluent::MultiEventStream.new
-    time = Time.parse("2015-05-22 11:22:33 UTC").to_i
-    es.add(time, {"message" => orig_message + invalid_utf8})
-
-    d = create_driver(CONFIG)
-    filtered_es = d.filter_stream('test.filter', es)
-    records = filtered_es.instance_variable_get(:@record_array)
-    assert_equal 1, records.length
-    assert_equal({"message" => orig_message + '?'}, records[0])
+    msg = {"message" => orig_message + invalid_utf8}
+    filtered = filter(CONFIG, [msg])
+    assert_equal([{"message" => orig_message + '?'}], filtered)
   end
 
 end
